d62d4f34803c8f55c408191714f287edba375186
akitekt <ericliu6688@gmail.com>
2013-08-21 14:58:50 -0400

    clean features for BDD

--- src/test/java/com/adp/test/second/main/ByMailStepdefs.java
+++ src/test/java/com/adp/test/second/main/ByMailStepdefs.java
@@ -1,45 +1,25 @@
 package com.adp.test.second.main;
 
 
-import cucumber.api.java.en.And;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
 
 public class ByMailStepdefs {
 
+    private Injector injector;
+    private Employee employee;
 
 
     @Given("^Employee of levelId (\\d+)$")
-    public void Employee_of_levelId(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-
-    }
-
-    @When("^Deliver PayStub ByMail$")
-    public void Deliver_PayStub_ByMail() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-
-    }
-
-    @Then("^Employee should receive PayStub of PayLevel (\\d+)$")
-    public void Employee_should_receive_PayStub_of_PayLevel(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
+    public void Employee_of_levelId(int levelId) throws Throwable {
+        injector = Guice.createInjector(new BootStrapModule());
+        employee = injector.getInstance(Employee.class);
+        employee.levelId = levelId;
 
     }
 
 
-    @And("^Employee should receive PayStub of PaymentAmount (\\d+)$")
-    public void Employee_should_receive_PayStub_of_PaymentAmount(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-
-    }
 
 
-    @And("^Paycheck is delivered through \"([^\"]*)\"$")
-    public void Paycheck_is_delivered_through(String carrier_name) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-
-    }
 }
--- target/test-classes/com.adp.test.second.main/ByMail.feature
+++ target/test-classes/com.adp.test.second.main/ByMail.feature
@@ -1,7 +1,6 @@
 Feature: Deliver the Paycheck
   Scenario: Deliver Paycheck correctly to Employee
     Given Employee of levelId 20
-    When Deliver PayStub ByMail
-    Then Employee should receive PayStub of PayLevel 20
-    And  Employee should receive PayStub of PaymentAmount 5000
-    And Paycheck is delivered through "UPS"
+#    When Deliver PayStub ByMail
+#    Then Employee should receive PayStub of PayLevel 20
+#    And  Employee should receive PayStub of PaymentAmount 5000
