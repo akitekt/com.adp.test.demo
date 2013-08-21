e80bfc9d7044e24eb19e451801934586b2ddeecc
akitekt <ericliu6688@gmail.com>
2013-08-21 15:09:16 -0400

    finished second bdd scenario

--- src/test/java/com/adp/test/second/main/ByMailStepdefs.java
+++ src/test/java/com/adp/test/second/main/ByMailStepdefs.java
@@ -3,12 +3,18 @@ package com.adp.test.second.main;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static junit.framework.Assert.assertEquals;
 
 public class ByMailStepdefs {
 
     private Injector injector;
     private Employee employee;
+    private PayStub paystub;
 
 
     @Given("^Employee of levelId (\\d+)$")
@@ -16,10 +22,25 @@ public class ByMailStepdefs {
         injector = Guice.createInjector(new BootStrapModule());
         employee = injector.getInstance(Employee.class);
         employee.levelId = levelId;
-
     }
 
 
+    @When("^Deliver PayStub ByMail$")
+    public void Deliver_PayStub_ByMail() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        paystub = employee.getPayStub();
+    }
 
 
+    @Then("^Employee should receive PayStub of PayLevel (\\d+)$")
+    public void Employee_should_receive_PayStub_of_PayLevel(int expected) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        assertEquals(expected,paystub.PayGradeId);
+    }
+
+    @And("^Employee should receive PayStub of PaymentAmount (\\d+)$")
+    public void Employee_should_receive_PayStub_of_PaymentAmount(int expectedAmount) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        assertEquals(expectedAmount,paystub.Payment,0.001);
+    }
 }
--- src/test/resources/com.adp.test.second.main/ByMail.feature
+++ src/test/resources/com.adp.test.second.main/ByMail.feature
@@ -1,6 +1,6 @@
 Feature: Deliver the Paycheck
   Scenario: Deliver Paycheck correctly to Employee
     Given Employee of levelId 20
-#    When Deliver PayStub ByMail
-#    Then Employee should receive PayStub of PayLevel 20
-#    And  Employee should receive PayStub of PaymentAmount 5000
+    When Deliver PayStub ByMail
+    Then Employee should receive PayStub of PayLevel 40
+    And  Employee should receive PayStub of PaymentAmount 4000
